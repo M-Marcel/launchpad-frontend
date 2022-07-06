9fed1ae29697f6646bcf27dbb7bd55dfd683ee6d
Logan <tushlogan@gmail.com>
2022-07-06 01:06:26 +0100

    first push

---
+++ src/img/index.js
@@ -0,0 +1,30 @@
+import explore from "./explore.svg";
+import twitter from "./twitter.svg";
+import discord from "./discord.svg";
+import medium from "./medium.svg";
+import telegramFooter from "./telegram-footer.svg";
+import mediumFooter from "./medium-footer.svg";
+import twitterFooter from "./twitter-footer.svg";
+
+import coinLeft from "./bg-coin-left.svg";
+import coinRight from "./bg-coin-right.svg";
+import color1 from "./eclipse-top-left.svg";
+import color2 from "./eclipse-top-right.svg";
+import coin from "./coin.png";
+import logo from "./logo.svg";
+
+export {
+  twitterFooter,
+  telegramFooter,
+  mediumFooter,
+  explore,
+  twitter,
+  medium,
+  discord,
+  coinLeft,
+  coinRight,
+  color1,
+  color2,
+  coin,
+  logo,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#04080F",
+        myblue: "#51B6F1",
+        myblue2: "#0e70aa",
+      },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        monument: ["Monument Extended", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
